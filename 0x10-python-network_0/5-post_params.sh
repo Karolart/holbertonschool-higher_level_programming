c4dc9edb8e1e55144097401d886f5a936c7255c2
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-09-07 18:58:29 -0500

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
