82f0ae7edb990b5322499c44bda90acab99e82e2
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-09-08 12:37:03 -0500

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,7 @@
 takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter
 """
+
 if __name__ == '__main__':
     import requests
     from sys import argv
