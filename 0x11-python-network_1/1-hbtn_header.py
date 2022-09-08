1f0a2d3d59909984432996754df53786eb335c80
Karolart <karoljuzz1610.com>
2022-09-07 21:41:43 -0700

    1. Response header value #0 ,2. POST an email #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+able File  11 lines (11 sloc)  363 Bytes
+
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
